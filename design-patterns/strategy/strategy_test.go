1ec3c50a02a0849fd33ceb3bfa19a2724942ceb4
aaronize <chendai2004@hotmail.com>
2019-07-30 14:50:27 +0800

    update

--- design-patterns/strategy/strategy_test.go
+++ design-patterns/strategy/strategy_test.go
@@ -1,7 +1,16 @@
 package strategy
 
-import "testing"
+import (
+    "log"
+    "testing"
+)
 
-func TestAddition_Compute(t *testing.T) {
+func TestNewArithmeticExecutor(t *testing.T) {
+    arithmetic := NewArithmeticCalculator(&Addition{})
+    ret := arithmetic.Calculate(10, 23)
+    log.Println(">>>addition result>>>", ret)
 
+    arithmetic2 := NewArithmeticCalculator(&Division{})
+    ret2 := arithmetic2.Calculate(10, 5)
+    log.Println(">>>division result>>>", ret2)
 }
